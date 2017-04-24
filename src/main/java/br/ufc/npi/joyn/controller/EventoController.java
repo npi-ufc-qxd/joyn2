b24f71208cc70ab7b481e9e465cd04a4c12284d2
Neto Deolino <netodeolino@gmail.com>
2017-04-24 17:17:27 -0300

    Cadastrar evento backend

--- src/main/java/br/ufc/npi/joyn/controller/EventoController.java
+++ src/main/java/br/ufc/npi/joyn/controller/EventoController.java
@@ -1,13 +1,16 @@
 package br.ufc.npi.joyn.controller;
 
-import java.sql.Date;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.ufc.npi.joyn.model.Evento;
 import br.ufc.npi.joyn.service.EventoService;
 
 @Controller
@@ -17,11 +20,19 @@ public class EventoController {
 	@Autowired
 	EventoService eventoService;
 	
-	@PostMapping(path="/cadastrar")
-	public String cadastrarEvento(@RequestParam String nome, @RequestParam String descricao, @RequestParam Date dataInicial,
-			@RequestParam Date dataFinal, @RequestParam Integer quantVagas, @RequestParam String local, @RequestParam boolean gamificacao){
+	@GetMapping(path="/cadastrar")
+	public ModelAndView cadastrarEventoFormulario(){
+		ModelAndView model = new ModelAndView("formCadastroEvento");
+		model.addObject("evento", new Evento());
 		
-		eventoService.cadastrarEvento(nome, descricao, dataInicial, dataFinal, quantVagas, local);
+		return model;
+	}
+	
+	@PostMapping(path="/cadastrar")
+	public String cadastrarEvento(@Valid Evento evento, BindingResult result){
+		if (result.hasErrors()) return "formCadastroEvento";
+		eventoService.cadastrarEvento(evento.getNome(), evento.getDescricao(), evento.getDataInicio(),
+				evento.getDataFim(), evento.getVagas(), evento.getLocal());
 		return "redirect:/formLoginUsuario";
 	}
 }
