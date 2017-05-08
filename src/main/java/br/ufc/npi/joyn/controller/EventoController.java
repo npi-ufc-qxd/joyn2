161076af0d1a4d4471d4489d68248de71675355c
MacilioFerreira <macilioferreira33@gmail.com>
2017-05-08 17:27:06 -0300

    Editar Eventos finalizado

--- src/main/java/br/ufc/npi/joyn/controller/EventoController.java
+++ src/main/java/br/ufc/npi/joyn/controller/EventoController.java
@@ -1,5 +1,8 @@
 package br.ufc.npi.joyn.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.ufc.npi.joyn.model.Evento;
+import br.ufc.npi.joyn.model.ParticipacaoEvento;
+import br.ufc.npi.joyn.model.Usuario;
 import br.ufc.npi.joyn.service.EventoService;
 
 @Controller
@@ -21,8 +26,12 @@ public class EventoController {
 	@Autowired
 	EventoService eventoService;
 	
+	@Autowired
+	UsuarioController usuarioController;
+	
 	@GetMapping(path="/salvar")
 	public ModelAndView salvarEventoFormulario(){
+
 		ModelAndView model = new ModelAndView("formCadastroEvento");
 		model.addObject("evento", new Evento());
 		
@@ -31,12 +40,18 @@ public class EventoController {
 	
 	@PostMapping(path="/salvar")
 	public String salvarEvento(@Valid Evento evento, BindingResult result){
-		System.err.println("CADRASTAR");
+		
+		Date data = new Date(); 
+		SimpleDateFormat formatador = new SimpleDateFormat("yyyy-MM-dd");
+		String data_atual = formatador.format(data);
+		
 		if (evento.getNome().equals("")) return "formCadastroEvento";
 		
 		if (result.hasErrors()) return "formCadastroEvento";
 		
 		if (evento.getDataFim().before(evento.getDataInicio())) return "formCadastroEvento";
+		
+		if (evento.getDataInicio().toString().compareTo(data_atual) < 0) return "formCadastroEvento";
 		
 		Evento salvo = eventoService.salvarEvento(evento);
 		return "redirect:/evento/" + salvo.getId();
@@ -44,7 +59,17 @@ public class EventoController {
 	
 	@GetMapping(path="/{id}")
 	public ModelAndView visualizarEvento(@PathVariable("id") Long id){
+		
 		Evento evento = eventoService.buscarEvento(id);
+		
+		Usuario usuarioLogado = usuarioController.getUsuarioLogado();
+		for (ParticipacaoEvento u : evento.getParticipantes()) {
+			if(u.getPapel().equals("ORGANIZADOR")){
+				if (u.getUsuario().equals(usuarioLogado)){
+					// Alguma coisa
+				}
+			}
+		} 
 		
 		ModelAndView model = new ModelAndView("detalhesEvento");
 		model.addObject("evento", evento);
@@ -61,9 +86,20 @@ public class EventoController {
 	}
 	
 	@PostMapping(path="/editar")
-	public String atualizar(Evento e){
-		System.err.println("EDITAR");
-		Evento evento = eventoService.salvarEvento(e);
-		return "redirect:/evento/"+evento.getId();
+	public String atualizar(Evento evento, BindingResult result){
+		Date data = new Date(); 
+		SimpleDateFormat formatador = new SimpleDateFormat("yyyy-MM-dd");
+		String data_atual = formatador.format(data);
+		
+		if (evento.getNome().equals("")) return "formEditarEvento";
+		
+		if (result.hasErrors()) return "formEditarEvento";
+		
+		if (evento.getDataFim().before(evento.getDataInicio())) return "formEditarEvento";
+		
+		if (evento.getDataInicio().toString().compareTo(data_atual) < 0) return "formEditarEvento";
+		
+		Evento evento_salvo = eventoService.salvarEvento(evento);
+		return "redirect:/evento/"+evento_salvo.getId();
 	}
 }
