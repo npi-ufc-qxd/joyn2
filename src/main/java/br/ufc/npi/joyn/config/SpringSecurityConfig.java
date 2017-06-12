3f016dc30e7b0a2696a948262e4a81841e5cdd2d
Tharsis Salathiel <salathiel_souzaviana@hotmail.com>
2017-06-12 14:24:33 -0300

    Remover url cadastro.

--- src/main/java/br/ufc/npi/joyn/config/SpringSecurityConfig.java
+++ src/main/java/br/ufc/npi/joyn/config/SpringSecurityConfig.java
@@ -28,7 +28,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         .authorizeRequests()
         	.antMatchers("/css/**", "/js/**", "/images/**", "/plugins/**", "/bootstrap/**", "/less/**").permitAll()
             .antMatchers("/usuario/cadastrar", "/usuario/starter", "/usuario/recuperarsenha",
-            		"/usuario/alterarsenha/**", "/usuario/novasenha","/usuariorest/cadastrar", "/usuariorest/csrf-token").permitAll()
+            		"/usuario/alterarsenha/**", "/usuario/novasenha", "/usuariorest/csrf-token").permitAll()
             .antMatchers("/api/**").hasRole("USUARIO")
             .anyRequest().authenticated()
             .and()
