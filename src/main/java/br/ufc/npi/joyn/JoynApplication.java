521f713f97662823707f41b11b0ad760a54234eb
Tharsis Salathiel <salathiel_souzaviana@hotmail.com>
2017-08-30 14:13:00 -0300

    Remoção do JWT da aplicação WEB.

--- src/main/java/br/ufc/npi/joyn/JoynApplication.java
+++ src/main/java/br/ufc/npi/joyn/JoynApplication.java
@@ -2,11 +2,7 @@ package br.ufc.npi.joyn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import br.ufc.npi.joyn.config.JwtFilter;
 
 @SpringBootApplication
 @Configuration
@@ -16,14 +12,4 @@ public class JoynApplication {
 		SpringApplication.run(JoynApplication.class, args);
 	}
 	
-	@Bean
-	public FilterRegistrationBean someFilterRegistration() {
-
-	    FilterRegistrationBean registration = new FilterRegistrationBean();
-	    registration.setFilter(new JwtFilter());
-	    registration.addUrlPatterns("/api/*");
-	    registration.setName("jwtFilter");
-	    registration.setOrder(Integer.MIN_VALUE);
-	    return registration;
-	}
 }
